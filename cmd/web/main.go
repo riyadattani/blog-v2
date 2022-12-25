9ed7ae69d208bdfa224c6ff090822aca04596eb9
Riya Dattani <riyaddattani@gmail.com>
2022-12-25 22:54:37 +0000

    ffs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"blog-v2/src"
 	"blog-v2/src/adapters/httpserver"
+	"fmt"
 	"log"
 	"os"
 
@@ -16,6 +17,12 @@ func main() {
 	config, err := loadAppConfig()
 	if err != nil {
 		log.Fatalf("failed to load config - %v", err)
+	}
+	dir, _ := os.Getwd()
+	entries, _ := os.ReadDir(dir)
+
+	for _, entry := range entries {
+		fmt.Print(entry.Name())
 	}
 
 	app := src.NewApp(ctx, os.DirFS("./cmd/web/posts"))
